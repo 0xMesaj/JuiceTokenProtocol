5b7545d113fa87ea9fc9ea5d07bb89e0b872336e
0xMesaj <77307474+0xMesaj@users.noreply.github.com>
2021-05-25 00:12:45 -0400

    init

--- contracts/interfaces/IJuiceTreasury.sol
+++ contracts/interfaces/IJuiceTreasury.sol
@@ -4,7 +4,5 @@ pragma solidity ^0.7.0;
 import "./IERC20.sol";
 
 interface IJuiceTreasury{
-    function sendEther(address payable, uint256) external;
-    function sendToken(IERC20, address, uint256) external;
-
+    function initializeTreasury( uint256 _amount ) external;
 }
