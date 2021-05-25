e86162bf91bb6a0a2aa7389f491192bbb2d6ebb7
0xMesaj <77307474+0xMesaj@users.noreply.github.com>
2021-05-25 04:02:11 -0400

    Update IxDAIAlternativeReceiver.sol

--- contracts/interfaces/IxDAIAlternativeReceiver.sol
+++ contracts/interfaces/IxDAIAlternativeReceiver.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.6.12;
 interface IxDAIAlternativeReceiver{
     function relayTokens(address _from, address _receiver, uint256 _amount) external;
     function relayTokens(address _receiver, uint256 _amount) external;
+    function relayTokens(address _receiver) external payable;
 }
